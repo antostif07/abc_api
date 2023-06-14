556e47b019f6f4ae3f017866adae5547d198582a
antostif07 <antostif07@gmail.com>
2023-06-14 17:48:40 +0100

    Course Updted

--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -4,11 +4,16 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CourseRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CourseRepository::class)]
 #[ORM\HasLifecycleCallbacks()]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['course.read']],
+    denormalizationContext: ['groups' => ['course.write']]
+)]
 class Course
 {
     #[ORM\Id]
@@ -30,6 +35,9 @@ class Course
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $cover = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
 
     #[ORM\PrePersist]
     public function updatedTimestamps()
@@ -99,6 +107,18 @@ class Course
     public function setCover(?string $cover): self
     {
         $this->cover = $cover;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
